b3bf8606e8765c076adadff09c8064b15b1d5180
VaibhavPandey0509 <vaibhav051422@gmail.com>
2026-03-02 18:54:35 +0530

    first

---
+++ doublell.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    public:
+    Node(int data1,Node*next1,Node*back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+
+
+    }
+};
+Node *convertarr2Dll(vector<int>&arr){
+    Node *head=new Node(arr[0]);
+    Node *prev=head;
+    for(int i=1;i<arr.size();i++){
+        Node *temp=new Node(arr[i],nullptr,prev);
+        prev->next=head;
+        prev=temp;
+    }
+    return head;
+
+
+}
+void print(Node *head){
+    Node *temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    
+}
+}
+int main(){
+    vector<int>arr={1,2,3,4,5};
+    Node *head=convertarr2Dll(arr);
+    print(head);
+    return 0;
+}
